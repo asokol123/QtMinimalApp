a674b5a2ca8ad102d1a115c392ef6d2c656f87dc
DimaTrushin <trushindima@yandex.ru>
2019-03-02 01:42:35 +0300

    Initial commit

---
+++ Qt/MainWindow.cpp
@@ -0,0 +1,41 @@
+﻿//---------------------------------------------------------------------------
+// Include
+//---------------------------------------------------------------------------
+
+#include "MainWindow.h"
+#include "ui_CMainWindow.h"
+//---------------------------------------------------------------------------
+
+namespace NSApplication {
+namespace NSQt {
+//---------------------------------------------------------------------------
+// CMainWindow Definition
+//---------------------------------------------------------------------------
+
+CMainWindow::CMainWindow(QWidget *parent) :
+  QMainWindow(parent),
+  Interface_(std::make_unique<Ui::CMainWindow>()) {
+  Interface_->setupUi(this);
+  show();
+}
+//---------------------------------------------------------------------------
+
+CMainWindow::~CMainWindow() {
+  // I need an explicit destructor because the class contains
+  // a unique_ptr to an incomplete type
+}
+//---------------------------------------------------------------------------
+
+Ui::CMainWindow* CMainWindow::accessUI() {
+  return Interface_.get();
+}
+//---------------------------------------------------------------------------
+
+const Ui::CMainWindow* CMainWindow::accessUI() const {
+  return Interface_.get();
+}
+//---------------------------------------------------------------------------
+} // NSQt
+//---------------------------------------------------------------------------
+} // NSApplication
+//---------------------------------------------------------------------------
---
+++ Qt/MainWindow.h
@@ -0,0 +1,38 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+//---------------------------------------------------------------------------
+// Include
+//---------------------------------------------------------------------------
+
+#include <QMainWindow>
+#include <memory>
+//---------------------------------------------------------------------------
+
+namespace Ui {
+class CMainWindow;
+}
+//---------------------------------------------------------------------------
+
+namespace NSApplication {
+namespace NSQt {
+//---------------------------------------------------------------------------
+// CMainWindow Declaration
+//---------------------------------------------------------------------------
+
+class CMainWindow : protected QMainWindow {
+  Q_OBJECT
+  using CBase = QMainWindow;
+public:
+  explicit CMainWindow(QWidget *parent = 0);
+  ~CMainWindow();
+  Ui::CMainWindow* accessUI();
+  const Ui::CMainWindow* accessUI() const;
+private:
+  std::unique_ptr<Ui::CMainWindow> Interface_;
+};
+//---------------------------------------------------------------------------
+} // NSQt
+//---------------------------------------------------------------------------
+} // NSApplication
+//---------------------------------------------------------------------------
+#endif // MAINWINDOW_H
